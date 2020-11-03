0ced39879f1df8e18c80f28840ca2f2020ea6f04
Andrew <amlefty5@gmail.com>
2020-11-03 13:46:16 -0500

    Beginnings

---
+++ Module_1/Mod1_Assignments/01.04_Student_Information/StudentInfoCard.java
@@ -0,0 +1,43 @@
+
+/**
+ * This program prints information that would normally be part of a student info card
+ *
+ * @Andrew Martin
+ * @
+ */
+public class StudentInfoCard
+{
+    /**
+     * This class prints out all of the data.
+     * @return student information
+     */
+    public static void main (String[] args)
+    {
+        System.out.println("**************************************************************");
+        System.out.println("*                                                            *");
+        System.out.println("*                   Student Information                      *");
+        System.out.println("*                                   *");
+        System.out.println("*************************************\n");
+        System.out.print("Name: Andrew Martin     ");
+        System.out.println("Birthday: May 1, 2003");
+        System.out.print("Go-by Name: Andrew      ");
+        System.out.print("Age: 17     ");
+        System.out.println("Grade: 12th\n");
+        System.out.print("School: Merrimack High School     ");
+        System.out.println("City: Merrimack, NH\n");
+        System.out.print("Home Phone: (603)429-3935     ");
+        System.out.println("Available: Fri-Tue 700 to 2000, Wed-Thurs 1600 to 2000");
+        System.out.print("Cell Phone:  (603)930-0523    ");
+        System.out.println("Best number to reach me, call or text");
+        System.out.println("Email: andrewmlefty5@gmail.com\n");
+        System.out.println("Mom's contact info");
+        System.out.println("Cell phone: (603)493-1879");
+        System.out.println("Email: annm603:comcast.net\n");
+        System.out.println("Dad's contact info");
+        System.out.println("Cell Phone: (603)493-2314");
+        System.out.println("Email: chuckm603:comcast.net\n");
+        System.out.println("Current Math Class: AP Calculus AB");
+        System.out.println("Programing Experience: Was in FIRST Lego League for 4 years\nWe used block programming. I am currently in my fourth year\nof FIRST Robotics working as a programmer. This year I am\nthe software lead working in Python for vision and Java for\ncontrolling the robot\n");
+        System.out.println("I am taking APCS because I am planning on going into either that\nor a similar field in college. I also already know a decent amount of java.");
+    }
+}
